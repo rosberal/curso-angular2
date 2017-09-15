13c65892091f85896f02a51895c301215a5f51f1
Rosber Almeida Junior <rosber.almeida@autotrac.corp>
2017-09-15 18:14:12 -0300

    1

---
+++ hello-typescript/main.js
@@ -0,0 +1,9 @@
+var minhaVar = 'minhavariavel';
+function minhafunc(x, y) { return x + y; }
+var nume = 2;
+var I = 3.14;
+var numeros = [1, 2, 3];
+numeros.map(function (valor) {
+    return valor * 2;
+});
+numeros.map(function (valor) { return valor * 2; });
---
+++ hello-typescript/main.ts
@@ -0,0 +1,13 @@
+var minhaVar='minhavariavel';
+
+function minhafunc(x,y) {return x+y;}
+
+
+let nume =2;
+const I=3.14;
+
+var numeros= [1,2,3];
+numeros.map(function(valor){
+    return valor *2;});
+
+numeros.map(valor=>valor*2);
