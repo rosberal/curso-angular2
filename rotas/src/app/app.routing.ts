99550b8854295b6f02bc141f43a2dc7b6cf9884a
Rosber Almeida Junior <rosber.almeida@AUTBSB>
2017-10-06 18:57:45 -0300

    sexta

--- rotas/src/app/app.routing.ts
+++ rotas/src/app/app.routing.ts
@@ -4,10 +4,10 @@ import { HomeComponent } from './home/home.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { LoginComponent } from './login/login.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
- 
+
 const APP_ROUTES: Routes = [
   { path: 'cursos', component: CursosComponent },
-  { path: 'curso/:id', component: CursoDetalheComponent },
+  { path: 'curso/:id' , component: CursoDetalheComponent },
   { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponent }
 ];
